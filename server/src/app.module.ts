457b664d22c60eee3a229c57ca9d9d71b1e740f3
iammarkps <markkung08@gmail.com>
2019-07-20 19:51:32 +0700

    :fire: Remove database module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
